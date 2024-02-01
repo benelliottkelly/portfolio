43173107b9aba9ab438a906145614628611c8208
Ben Kelly <benelliottkelly@gmail.com>
2024-02-01 13:00:22 +0000

    added useInView to about me and animations that trigger on page coming into view

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,42 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { useInView } from 'react-intersection-observer'
 import profileImg from '../assets/images/BenProfile.jpg'
 
 export default function AboutMe() {
 
-  // const [ onScreen, setOnScreen ] = useState(false)
-
-  // function isInViewPort() {
-  //   const aboutMeScreen = document.querySelector('section#about-me')
-  //   const rect = aboutMeScreen.getBoundingClientRect()
-  //   // console.log(rect)
-  //   console.log('innerwidth', window.innerWidth)
-  //   console.log('innerheight', window.innerHeight)
-  //   if (
-  //     parseInt(rect.top) >= 0 &&
-  //     // parseInt(rect.left) >= 0 &&
-  //     parseInt(rect.bottom) <= (parseInt(window.innerHeight) || parseInt(document.documentElement.clientHeight)) // &&
-  //     // parseInt(rect.right) <= (parseInt(window.innerWidth) || parseInt(document.documentElement.clientWidth))
-  //   ) {
-  //     setOnScreen(true)
-  //   } else {
-  //     setOnScreen(false)
-  //   }
-  // }
-
-  // document.addEventListener('scroll', isInViewPort)
+  const { ref: aboutMeRef, inView: aboutMeInView } = useInView()
 
   return(
-    <section className="screen" id='about-me'>
+    <section className="screen" id='about-me-container'>
       <h2>About Me</h2>
-      <div className="about-me-contents">
+      <div ref={aboutMeRef} className={`about-me-contents ${aboutMeInView ? 'animate-about-me' : ''}`}>
         <div className='playful'>
-          <img src={profileImg} />
+          <div className='image-wrap'>
+            <img src={profileImg}/>
+          </div>
           <div className='spotify-container'>
             <h3>Listen along with my coding playlist...</h3>
             <iframe style={{borderRadius: '12px'}} src="https://open.spotify.com/embed/playlist/3PMblW80YDdbgh6bbfq9vC?utm_source=generator" width="100%" height="152" frameBorder="0"  allowfullscreen='' allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
           </div>
         </div>
-        <div>
+        <div className='statement-skills'>
           <p>Brand statement</p>
           <article>
             <ul>
